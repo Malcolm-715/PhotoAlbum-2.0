e666ba4b7554834507f566237f29abc0dd2aa022
Malcolm <visagiemalcolm22@gmail.com>
2021-11-20 16:41:22 +0200

    latest

--- loginDB.php
+++ loginDB.php
@@ -11,17 +11,23 @@ if(!$conn){
         $email = $_POST['user_email'];
         $password = $_POST['user_password'];
         $image = $_FILES['user_image']['name'];
-    
-        $query = "INSERT INTO register(user_name, user_email, user_password, user_image) VALUES('$name',' $email',' $password',' $image')";
-        $query_run = mysqli_query($conn, $query);
-    
-        if($query_run){
-            move_uploaded_file($_FILES["user_image"]["tmp_name"], "uploads/".$_FILES["user_image"]["name"]);
-            $_SESSION['status'] = "Image Successfully Uploaded!";
+
+        if(file_exists("uploads/".$_FILES["user_image"]["name"])){
+            $filename = $_FILES["user_image"]["name"];
+            $_SESSION['status'] = "Image Already Exists! ".$filename;
             header('Location: login.php');
         }else{
-            $_SESSION['status'] = "Image Not Uploaded!";
-            header('Location: login.php');
+            $query = "INSERT INTO register(user_name, user_email, user_password, user_image) VALUES('$name',' $email',' $password',' $image')";
+            $query_run = mysqli_query($conn, $query);
+            
+            if($query_run){
+                move_uploaded_file($_FILES["user_image"]["tmp_name"], "uploads/".$_FILES["user_image"]["name"]);
+                $_SESSION['status'] = "Image Successfully Uploaded!";
+                header('Location: login.php');
+            }else{
+                $_SESSION['status'] = "Image Not Uploaded!";
+                header('Location: login.php');
+            }
         }
     }
 }
